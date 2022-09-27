a3ae231fcf5407482f08d4fd436507d1143e4b69
IgnacioSicaja <109763667+IgnacioSicaja@users.noreply.github.com>
2022-09-27 01:43:50 -0600

    Add files via upload

---
+++ PI/Principal1.php
@@ -0,0 +1,199 @@
+<!doctype html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <link rel="stylesheet" href="Principal2.css">
+  </head>
+  <body>
+        <SECTIOn>
+            <div>
+              <nav class="navbar"></nav>
+            <nav class="navbar navbar-expand-lg bg-light">
+      <div class="container">
+        <a class="navbar-brand" href="#1"><img src="logo.png" class="logo" alt=""> Concepto</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#2">  Tipos de desechos</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#3">Orgánicos</a>
+            <li class="nav-item">
+              <a class="nav-link" href="#4">Inorgánicos</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#5">Sanitarios</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Aprendé más! 
+              </a>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="frenar.php">Qué es el reciclaje</a></li>
+                
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+        </div>
+    <section id="1" class="container">
+      <div> 
+        <a href="#Home" id="#home"></a>
+        <br>
+        <br>
+        <br>
+        <br>
+        <h1>La contaminación en mi comunidad</h1>
+        
+        <br>
+        <div class="container">    
+        <p>La contaminación en mi comunidad es un problema porque la gente no se espera para tirarla en botes de basura, si no que la tira en la calle, caminos, veredas, orillas o en arbustos, etcétera. La basura se clasifica en tres diferentes categorías: basura orgánica, basura inorgánica y basura sanitaria.</p>
+        <p>Los problemas ambientales suelen afectar más a las áreas urbanas: las ciudades, sean más grandes o pequeñas, concentran un mayor número de población y espacios industriales, producen más residuos, tienen un índice de vehículos más alto, los espacios naturales son más pequeños, el aire está contaminado, entre otras cuestiones. ¡Y todo esto, claro está, incide en nuestra salud y bienestar!</p>
+        </div>
+        <br>        
+        <img src="basura.jpg" class="basurauno" alt="">
+        <br>
+        <br>
+        <section id="2" >
+        <h5>Primero que nada, es importante saber clasificar el tipo de basura e informarse del tema, para ello hemos escrito su clasificación con sus ejemplos. Esto con el fin y objetivo de educar a las personas de nuestra comunidad.</h5>
+      </div>
+      
+    </section>
+    
+    
+
+    
+      
+      
+      <br>
+      <br>
+      <div class="container">
+        <h1>Clasificación de los desechos</h1>
+        <p>Se clasifican en tres tipos:</p>
+        <ol>Basura orgánica</ol>
+        <ol>Basura inorgánica</ol>
+        <section id="3"></section>
+        <ol>Basura sanitaria</ol>
+        <br>
+        <br>
+        
+        
+        <h2>Basura orgánica</h2>
+        
+        <p>Se genera de los restos de seres vivos como plantas y animales.</p>
+        
+        <img src="basurainorganica.jpg" class="basura2" alt="">
+        <ol> <h3>Ejemplos</h3>        
+        </ol>
+          <li>Cáscaras de frutas y verduras</li>
+          <li>Cascarones</li>
+          <li>Restos de alimentos</li>
+          <li>Huesos</li>
+          <li>Papel y telas naturales como la seda, el lino y el algodón</li>
+          <br>
+          <section id="4" >
+        <p>Este tipo de basura es biodegradable.</p>  
+      </div>    
+    </section>      
+      <br>
+      <br>
+      
+
+        
+        <div class="container ">
+          <h2>Basura inorgánica</h2>
+            
+            <p>Proviene de minerales y productos sintéticos.</p>
+            <img src="inorganico.jpeg" class="basura3" alt="">
+            
+            <ol><h3>Ejemplos</h3></ol>
+            
+            <li>Metales</li>
+            <li>Plástico</li>
+            <li>Vidrio</li>
+            <li>Cartón plastificado</li>
+            <li>Telas sintéticas</li>
+            <br>
+            <p>Dichos materiales no son agradables para el medio ambiente.</p>
+            <section id="5" >
+        </div>
+    </section>
+        <br>
+        <br>
+        <br>
+        <br>
+      <div class="container">
+        <h2>Basura sanitaria</h2>
+        <p>Son los materiales utilizados para realizar curaciones médicas.</p>
+        <ol><h3>Ejemplos</h3></ol>
+        <img src="residuos.jpg" class="basura4" alt="">
+        <li>Gasas</li>
+        <li>Vendas o algodón</li>
+        <li>Papel higiénico</li>
+        <li>Toallas sanitarias</li>
+        <li>Pañuelos y pañales desechables</li>
+        <br>
+        <p>Cabe aclarar que los residuos sanitarios u hospitalarios son aquellos generados en las actividades sanitarias.</p>
+        
+        <p>Buena parte de ellos se encuentran potencialmente contaminados con sustancias o líqidos biológicos, al haber sido utilizados en tramientos clínicos.</p>
+        
+        <h5>Los residuos sanitarios, de tratamiento especial, proceden de:</h5>
+
+        <li>Hospitales</li>
+        <li>Clínicas y sanatorios de medicina humana</li>
+        <li>Consultas de profesionales independientes</li>
+        <li>Centros de asistencia social</li>
+        <li>Laboratorios de investigación médica</li>
+        <li>Centros de atención primaria</li>
+        <li>Centros de salud</li>
+        <li>Centros de planificación</li>
+        <li>Cualquier otra que tenga relación con la salud humana</li>
+        <li>Centros y servicios veterinarios asistenciales</li>
+        <br>
+        
+
+
+      </div>
+    </section>
+
+
+
+
+    <section>
+      <div>
+        
+      </div>
+    </section>
+
+    <section>
+    </section>
+
+        <section class="fondo">
+            <div  class="datos">
+                <div class="container">
+                  <br>
+                  <h5 class="nombre">Jose Ignacio Sicajá Guzmán
+                    <br><br>5to Bachillerato en computación <br><br>  </h5>
+                    <br>
+                    <p class="numero">+502 3793 0537    |    jose.sicaja@colegiocbcelcarmen.edu.gt    |   </p>
+                    <br>
+                    
+                    
+                    <h5 class="tema">La contaminación en mi comunidad</h5>
+                    <p class="autor">©  Todos los derechos reservados     |     Diseñado por J. Ignacio S. Guzmán</p> 
+                    <br>
+                </div>
+                
+            </div>
+        </section>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ PI/frenar.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <link rel="stylesheet" href="frenar.css">
+  </head>
+  <body>
+    <div>
+                <nav class="navbar"></nav>
+                <nav class="navbar navbar-expand-lg bg-light">
+        <div class="container">
+            <a class="navbar-brand" href="Principal1.php"><img src="logo.png" class="frenar" alt=""> Volver a página principal</a>
+            
+                </ul>
+                </li>
+            </ul>
+            </div>
+        </div>
+        </nav>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+        <section>
+            <div class="container">
+                <br>
+                <br>
+                <br><br>
+                <h1>¿Qué es el reciclaje y cómo puede ayudar a tener una comunidad más limpia y saludable?</h1>
+                <br>
+                <p>Dicha expresión hace referencia a un proceso cuyo objetivo es convertir residuos en nuevos productos o en materia prima para su posterior utilización. </p>
+                
+                <p>El reciclar es una especie de muletilla que opera
+                    muy bien cuando se indaga respecto a soluciones a los
+                    asuntos y problemas ecológicos y ambientales y, ha venido a desplazar a los dos primeros elementos del triángulo ecológico: se necesita ir más al fondo para que el
+                    individuo se acuerde de que también existe la reducción
+                    y la reutilización.</p>
+                <h4>¿Cómo ayuda el reciclaje a nuestra comunidad?</h4>
+                <p>Reciclar conlleva ahorrar materias primas, energía, agua y reducir las emisiones de gases de efecto invernadero. Y además, genera nuevos puestos de trabajo y tejido industrial.
+
+                    Sin duda, los beneficios de reciclar nuestros residuos son enormes en comparación con la acción tan sencilla y cotidiana que supone.</p>
+                <p>Está claro que en nuestros hogares hay muchas cosas que no usamos y que podemos llegar a considerar como basura, pero no todo es basura puesto que muchas de esas cosas aún se le puede dar un segundo uso!</p>
+                    <img src="reciclaje.jpg" class="reciclaje" alt="">
+                <br>
+                <p>Enseñemos a nuestros niños y jóvenes a tener cultura de limpieza y ser ordenados con tirar la basura en los lugares correspondientes y no en las calles. </p>
+
+            </div>
+        </section>
+</body>
+</html>
